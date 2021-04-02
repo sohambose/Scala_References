d7a94c3f0206f5d0213878a6eab390bf5477eea7
sohambose <sohamboseofficial@gmail.com>
2021-04-02 20:40:34 +0530

    My Generic List Iplementation

--- src/lectures/OOP/Generics.scala
+++ src/lectures/OOP/Generics.scala
@@ -1,5 +1,44 @@
 package lectures.OOP
 
-object Generics extends App {
+import lectures.OOP.Inheritance.Animal
 
+object Generics extends App {
+  class MyList[+T]{
+    //use type T inside the class definition
+    def add[T1 >: T](element: T1): MyList[T1] = ???
+  }
+
+  val lstIntegers= new MyList[Int]
+  val lstStrings= new MyList[String]
+
+  //---Generic Methods
+  object MyList {
+    def empty[T]: MyList[T] = ???
+  }
+
+  //Variance Problem:
+  class cat extends  Animal
+  class dog extends  Animal
+
+  //----1----Covariance:(List of cat can extend List of Animals)
+  class covariantList[+T]
+  val animal: Animal = new cat
+  val animalList: covariantList[Animal]= new covariantList[cat]
+
+  //----2----Invariance: (List of Cat and List of Animals are different)
+  //No substitution is allowed.
+  class InvariantList[T]
+  val invariantList: InvariantList[Animal] = new InvariantList[Animal]
+
+  //----3----Contravariance: ()
+  class ContravarientList[-T]
+  val ContravarientList: ContravarientList[cat]= new ContravarientList[Animal]
+
+  //--Bounded Types--
+  class Cage[T<:Animal](animal: T)
+  val cage= new Cage(new dog)
+
+ /* class Car
+  val newcage= new Cage(new Car)
+*/
 }
